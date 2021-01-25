3fff516ebe416b17ec8bd356da384581cd6823b0
MelkdeSousa <melk.sousap@gmail.com>
2021-01-25 16:00:27 -0300

    Home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,38 @@
 import React from 'react'
 
-import { Container } from '../styles/pages/Home'
+import assets from '../assets/assets.json'
+
+import { QuizContainer, QuizBackground } from '../styles/components/Quiz'
+import {
+  Widget,
+  WidgetHeader,
+  WidgetContent
+} from '../styles/components/Widget'
+import GithubCorner from '../styles/components/GithubCorner'
+import Footer from '../styles/components/Footer'
 
 const Home: React.FC = () => {
-  return <Container></Container>
+  return (
+    <QuizBackground backgroundImage={assets.bg}>
+      <QuizContainer>
+        <Widget>
+          <WidgetHeader>
+            <h1>Lorem ipsum dolor sit amet </h1>
+          </WidgetHeader>
+          <WidgetContent>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+          </WidgetContent>
+        </Widget>
+        <Widget>
+          <WidgetContent>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+          </WidgetContent>
+        </Widget>
+        <Footer />
+      </QuizContainer>
+      <GithubCorner projectUrl="https://github.com/MelkdeSousa" />
+    </QuizBackground>
+  )
 }
 
 export default Home
