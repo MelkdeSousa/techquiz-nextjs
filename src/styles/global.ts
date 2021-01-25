41f26cd782f41761dc1d2657f97407b3e4b2f9a6
MelkdeSousa <melk.sousap@gmail.com>
2021-01-25 12:58:50 -0300

    preset to global theme

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -2,10 +2,22 @@ import { createGlobalStyle } from 'styled-components'
 
 export default createGlobalStyle`
   * {
-    margin: 0;
-    padding: 0;
     box-sizing: border-box;
   }
-
-  body {}
+  body {
+    margin: 0;
+    padding: 0;
+    display: flex;
+    flex-direction: column;
+    font-family: 'Lato', sans-serif;
+    color: ${({ theme }) => theme.colors.contrastText};
+  }
+  html, body {
+    min-height: 100vh;
+  }
+  #__next {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+  }
 `
