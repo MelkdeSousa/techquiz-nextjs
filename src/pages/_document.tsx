3ee07f83a9c986e9d4798afe503f784e881fcb73
MelkdeSousa <melk.sousap@gmail.com>
2021-01-25 16:57:39 -0300

    add meta tags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -47,6 +47,40 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css?family=Roboto:400,500,700"
             rel="stylesheet"
           />
+
+          {/* Primary Meta Tags */}
+          <title>TechQuiz</title>
+          <meta name="title" content="TechQuiz" />
+          <meta
+            name="description"
+            content="Um quiz para quem pensa que sabe de algo sobre Tech."
+          />
+
+          {/* Open Graph / Facebook */}
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://techquiz.vercel.app/" />
+          <meta property="og:title" content="TechQuiz" />
+          <meta
+            property="og:description"
+            content="Um quiz para quem pensa que sabe de algo sobre Tech."
+          />
+          <meta
+            property="og:image"
+            content="https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"
+          />
+
+          {/* Twitter */}
+          <meta property="twitter:card" content="summary_large_image" />
+          <meta property="twitter:url" content="https://techquiz.vercel.app/" />
+          <meta property="twitter:title" content="TechQuiz" />
+          <meta
+            property="twitter:description"
+            content="Um quiz para quem pensa que sabe de algo sobre Tech."
+          />
+          <meta
+            property="twitter:image"
+            content="https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"
+          />
         </Head>
         <body>
           <Main />
