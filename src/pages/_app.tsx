3a9e60145e6a4155abfd28baef0bc8040d655dcd
MelkdeSousa <melk.sousap@gmail.com>
2021-01-26 21:06:01 -0300

    add font Lato

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Head from 'next/head'
 import { AppProps } from 'next/app'
 import { ThemeProvider } from 'styled-components'
 
@@ -7,10 +8,19 @@ import theme from '../styles/theme'
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
-    <ThemeProvider theme={theme}>
-      <Component {...pageProps} />
-      <GlobalStyle />
-    </ThemeProvider>
+    <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+        <GlobalStyle />
+      </ThemeProvider>
+    </>
   )
 }
 
